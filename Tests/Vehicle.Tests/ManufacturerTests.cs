b60b1f32b6e34914ca13c8e9a0aeeb126f615e8f
Nedyalko Nedyalkov <nedvnedyalkov@gmail.com>
2025-11-20 11:59:10 +0200

    feat: Add Tests.
    + Make internals visible to the test assembly.
    + Test assembly visibility constraints. Don't allow users of the library to create objects themselves. Hide constructors. Expose factory and Instances.
    + Test creation of internal types and check for exception handling.
    + When testing factory don't expect exceptions anymore.

---
+++ Tests/Vehicle.Tests/ManufacturerTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection;
+
+namespace Vehicle.Tests
+{
+    [TestClass]
+    public sealed class ManufacturerTests
+    {
+        [TestMethod]
+        public void Manufacturer_MultipleCarsWithSameManufacturer_ShareTheSameManufacturer()
+        {
+            var car1 = new Car(CarManufacturers.Toyota.Value);
+            var car2 = new Car(CarManufacturers.Toyota.Value);
+
+            Assert.AreSame(car1.Manufacturer, car2.Manufacturer);
+        }
+
+        [TestMethod]
+        public void Manufacturers_DontHaveNonPrivateConstructors()
+        {
+            var assembly = Assembly.GetAssembly(typeof(IManufacturer));
+            Assert.IsNotNull(assembly, "Could not get assembly containing IManufacturer.");
+
+            var manufacturerTypes = assembly.GetTypes().Where(t => typeof(IManufacturer).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+            Assert.IsTrue(manufacturerTypes.Count() >= 1);
+
+            foreach (var manufacturerType in manufacturerTypes)
+            {
+                var constructors = manufacturerType.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                Assert.IsTrue(constructors.Count() >= 1);
+
+                foreach (var constructor in constructors)
+                {
+                    Assert.IsTrue(constructor.IsPrivate, $"Manufacturer type {manufacturerType.FullName} has a non-private constructor.");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Manufacturers_InstancePropertiesAreNotPublic()
+        {
+            var assembly = Assembly.GetAssembly(typeof(IManufacturer));
+            Assert.IsNotNull(assembly, "Could not get assembly containing IManufacturer.");
+
+            var manufacturerTypes = assembly.GetTypes().Where(t => typeof(IManufacturer).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+            Assert.IsTrue(manufacturerTypes.Count() >= 1);
+
+            foreach (var manufacturerType in manufacturerTypes)
+            {
+                var properties = manufacturerType.GetProperties(BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+                Assert.IsTrue(properties.Count() >= 1);
+
+                foreach (var property in properties)
+                {
+                    if (property.Name.Contains("Instance", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        Assert.IsFalse(property.GetMethod?.IsPublic == true, $"Manufacturer type {manufacturerType.FullName} has a public instance property!");
+                    }
+                }
+            }
+        }
+    }
+}
