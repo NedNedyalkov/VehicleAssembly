d4d556d83786fc66b28b63716f10ce27871ae2e5
Nedyalko Nedyalkov <nedvnedyalkov@gmail.com>
2025-11-23 20:16:46 +0200

    documentation: Improve DebugLogger documentation.
    + It now reflects nuance about it being a record. The concrete example is that one Car is equal to another Car if they're from the same manufacturer and with the same tires.

--- VehicleAssembly/Services/DebugLogger.cs
+++ VehicleAssembly/Services/DebugLogger.cs
@@ -3,6 +3,11 @@ using VehicleAssembly.Abstractions;
 
 namespace VehicleAssembly.Services
 {
+    /// <summary>
+    /// Provides a logger implementation that writes messages to the debug output.
+    /// </summary>
+    /// <remarks>This logger is a record intentionly. This way it will not be considered when doing
+    /// comparison between other records, that keep loggers</remarks>
     public record DebugLogger() : ILogger
     {
         public void WriteLine(string message) => Debug.WriteLine(message);
