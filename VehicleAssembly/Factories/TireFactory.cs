9d147f0e173bfe9cc87f1b35b9f5eec9e00b07f5
Nedyalko Nedyalkov <nedvnedyalkov@gmail.com>
2025-11-23 15:34:17 +0200

    feat: Add TireFactory
    + Make a factory the only way to create tires outside of the assembly.

---
+++ VehicleAssembly/Factories/TireFactory.cs
@@ -0,0 +1,33 @@
+﻿using VehicleAssembly.Domain.Tires;
+
+namespace VehicleAssembly.Domain.Vehicles
+{
+    public static class TireFactory
+    {
+        public static bool TryCreateSummerTire(float pressureBar, float maxTemperatureC, out SummerTire? tire)
+        {
+            try
+            {
+                tire = new SummerTire(maxTemperatureC: maxTemperatureC, pressureBar: pressureBar);
+            }
+            catch
+            {
+                tire = null;
+            }
+            return tire is not null;
+        }
+
+        public static bool TryCreateWinterTire(float pressureBar, float minTemperatureC, float thicknessCm, out WinterTire? tire)
+        {
+            try
+            {
+                tire = new WinterTire(minTemperatureC: minTemperatureC, thicknessCm: thicknessCm, pressureBar: pressureBar);
+            }
+            catch
+            {
+                tire = null;
+            }
+            return tire is not null;
+        }
+    }
+}
